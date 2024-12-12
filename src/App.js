1730025a3ce72e921d1535705b668734134fd905
Iana <iana.li.timofeevna@gmail.com>
2024-12-12 18:45:39 +0100

    [fix deployment error]

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { Routes, Route } from 'react-router-dom';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Layout />} >
+      <Route path="/kasa/" element={<Layout />} >
         <Route />
         <Route index element={<MainPage />} />
         <Route path='/location/:id' element={<LocationPage />} />
--- src/components/error/Error.jsx
+++ src/components/error/Error.jsx
@@ -7,7 +7,7 @@ export default function Error() {
         <div className='error-page'>
             <h1 className='error-title'>404</h1>
             <h3 className='error-description'>Oups! La page que vous demandez n'existe pas.</h3>
-            <Link to='/'>
+            <Link to='/kasa/'>
                 <p className='error-link'>Retourner sur la page d’accueil</p>
             </Link>
         </div>
