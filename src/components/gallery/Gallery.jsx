8f1cbda277ee369f89adb99cf8d493da9dc7d460
Iana <iana.li.timofeevna@gmail.com>
2024-12-16 11:51:49 +0100

    [add check if in the pictures array (gallery) more than 1 image]

--- src/components/gallery/Gallery.jsx
+++ src/components/gallery/Gallery.jsx
@@ -6,9 +6,6 @@ import './Gallery.css'
 export default function Gallery({ pictures }) {
 
     const [currentPicture, setCurrentPicture] = useState(0)
-    const picture = pictures[0]
-
-    console.log(picture)
 
     const handleNext = () => {
         setCurrentPicture((prevIndex) => (prevIndex + 1) % pictures.length)
@@ -20,11 +17,13 @@ export default function Gallery({ pictures }) {
 
     return (
         <div className='gallery-list'>
-            <div className='button-gallery'>
-            <button className='chevron-left' onClick={handlePrev}> <FontAwesomeIcon icon={faChevronLeft} className="custom-size" /></button>
-            <button className='chevron-right' onClick={handleNext}><FontAwesomeIcon icon={faChevronRight} className="custom-size" /></button>
-            </div>
-                <img src={pictures[currentPicture]} alt={`Picture-${currentPicture}`} />
+            {pictures.length > 1 && (
+                <div className='button-gallery'>
+                    <button className='chevron-left' onClick={handlePrev}> <FontAwesomeIcon icon={faChevronLeft} className="custom-size" /></button>
+                    <button className='chevron-right' onClick={handleNext}><FontAwesomeIcon icon={faChevronRight} className="custom-size" /></button>
+                </div>
+            )}
+            <img src={pictures[currentPicture]} alt={`Picture-${currentPicture}`} />
         </div>
     )
 }
